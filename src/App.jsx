a1d176a1d39b267b42193c784b88f646f8c83b4c
Gaurab <gaurabkus781@gmail.com>
2025-06-15 15:12:36 +0530

    Add functions (add,update,toggle,delete) in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
-import { useState } from 'react'
-import { TodoProvider } from './context'
+import { useEffect, useState } from "react";
+import { TodoProvider } from "./context";
 
 const App = () => {
-  const [todos, setTodos] = useState([])
-  
+  const [todos, setTodos] = useState([]);
+
   const addTodo = (todo) => {
-    setTodos(prev => [ {todo, ...prev}])
-  }
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === todo.id ? todo : prevTodo))
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodo = (id) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  // Get the data from localStorage
+  useEffect(() => {
+    const stored = localStorage.getItem('todos');
+    if(stored) {
+      setTodos(JSON.parse(stored))
+    }
+  }, []);
+
   return (
-    <TodoProvider value={{ }}>
+    <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleTodo }}>
       hello
     </TodoProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
